61f18750646c04090cbe8cdf568f814035e415ed
nanamontford <adwowamontford@gmail.com>
2026-01-18 14:41:29 +0000

    add player and tiles to js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let playerO = "O";
+let playerrX = "X";
+let CurrentPlayer = "O";
+
+//               0   1   2   3   4   5   6   7   8
+let tileGrid = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+let tileCell;
